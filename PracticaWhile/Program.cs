861d9658644142bc407cfe3d35b99ea122c6b6a2
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 01:15:55 -0700

    Agregar archivos de proyecto.

---
+++ PracticaWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña1, contraseña2 = " ";
+            Console.Write("Escribe tu contraseña: ");
+            contraseña1 = Console.ReadLine();
+            Console.ReadKey();
+
+            while (contraseña1 != contraseña2)
+            {
+                Console.Write("Confirma tu contraseña: ");
+                contraseña2 = Console.ReadLine();
+                Console.ReadKey();
+            }
+            Console.WriteLine("!Contraseña Guardada!");
+            Console.ReadKey();
+
+        }
+    }
+}
